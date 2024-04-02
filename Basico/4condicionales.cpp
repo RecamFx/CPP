38892f4129a24a939e9818cf81c66a984a9dc6b7
RecamFx <camiloreartesgm@gmail.com>
2024-04-02 13:23:03 -0300

    Automatic Terminal

--- Basico/4condicionales.cpp
+++ Basico/4condicionales.cpp
@@ -7,9 +7,11 @@ int main(){
     
     // IF:
     // ESTRUCTURA: if(condicion){codigo}
+    // Los ifs ejecutan codigo si la condicion es verdadera o se cumple (True)
+
     // Se le puede agregar else: que dice que si no se cumple la funcion pasa a eso
     // Tambien se le pueden agregar los else if, que son elses normales con un if para verificar
-    // Los ifs ejecutan codigo si la condicion se cumple
+
     // En este ejemplo vamos a ver si la edad de pepito es mayor a 18
     // Si es asi vamos a escribir que es mayor, si no es asi vamos a escribir que es menor.
 
@@ -23,5 +25,23 @@ int main(){
 
     // En este codigo verificamos si pepito tiene mas o menos de 18 de edad, en el caso de ninguno ser correcto pasa al else, porque?
     // Porque si no es mas o menos de 18 entonces es 18
+
+
+
+    //todo----------------------------------------------------------------------------------------------------------------------------------//
+
+    // WHILE:
+    // ESTRUCTURA: while(condicion){codigo} 
+    // Los while se ejecutan en bucle mientras la condicion sea verdadera o se cumpla (True)
+
+
+    // En este ejemplo queremos crear un while para decirle que mientras la variable numero sea menor a diez que se imprima el valor y se le sume 1
+    int numero = 0;
+
+    while(numero < 10){
+        cout<<"El numero es: "<<numero<<endl;
+        numero++; // Usamos ++ para indicar que queremos que se incremente en 1 la variable numero
+    }
+
 
 }
