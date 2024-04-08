026305a243f94f1e713423b5834994d1ff911394
RecamFx <camiloreartesgm@gmail.com>
2024-04-07 22:54:34 -0300

    For estructura

---
+++ Basico/7_estructura_for.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // Estructura del for
+    // for(inicializacion; condicion; actualizacion) {codigo}
+
+    // Inicializacion: creas una variable con valor x. EJEMPLO: int i = 0;
+    // Condicion: creas una condicion que mientras se cumpla el for funciona. EJEMPLO = i<10
+    // Actualizacion: actualizamos la variable de inicializacion. EJEMPLO: i++
+
+    for(int i = 0; i<10; i++){
+        cout<<i<<endl;
+    }
+    //? 0
+    //? 1
+    //? 2
+    //? 3
+    //? 4
+    //? 5
+    //? 6
+    //? 7
+    //? 8
+    //? 9
+
+}
